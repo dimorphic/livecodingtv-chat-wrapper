d0360acd03620ad94aad3c32a110b87564dfd0df
Sabin Tudor <sabin@gmx.us>
2015-12-19 18:58:33 +0200

    Create commands dir and add base index and public help command

---
+++ src/commands/index.js
@@ -0,0 +1,11 @@
+//
+//  Commands
+//  each command is a pure function
+//  ...that receives the Client as an arg
+//
+import help from './help';
+
+// export commands
+module.exports = {
+    help
+};
